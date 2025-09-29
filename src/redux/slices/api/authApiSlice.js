87e4cd1013cf5ea8fd65d793364d4902b84cb53b
Default-ant <krishnamani468@gmail.com>
2025-09-29 20:12:25 +0000

    blank page error

--- src/redux/slices/api/authApiSlice.js
+++ src/redux/slices/api/authApiSlice.js
@@ -5,11 +5,19 @@ export const authApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (data) => ({
-        url: `${USERS_URL}/login`,
-        method: "POST",
-        body: data,
-        credentials: "include",
-      }),
+       url: `${USERS_URL}/login`,
+      method: "POST",
+      body: data,
+      credentials: "include",
+     }),
+      async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+      try {
+        const { data } = await queryFulfilled;
+        dispatch(setCredentials(data.user)); // ✅ update Redux
+      } catch (err) {
+      console.error("Login failed:", err);
+        }
+      },
     }),
     register: builder.mutation({
       query: (data) => ({

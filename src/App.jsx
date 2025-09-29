9158e339a0ec81388f1e2201e26bdf6d237772cd
Default-ant <krishnamani468@gmail.com>
2025-09-29 21:27:25 +0000

    blank page error

--- src/App.jsx
+++ src/App.jsx
@@ -15,30 +15,12 @@ import {
   StatusPage,
 } from "./pages";
 import { setOpenSidebar } from "./redux/slices/authSlice";
-import { Outlet } from "react-router-dom";
 
-return (
-  <div>
-    {/* header/sidebar */}
-    <Outlet /> {/* ✅ renders child route */}
-  </div>
-);
-
-const Layout() {
+function Layout() {
   const { user } = useSelector((state) => state.auth);
   const location = useLocation();
 
-  // ✅ Wait for Redux to hydrate
-  if (user === undefined) {
-    return null; // or a loading spinner
-  }
-
-  // ✅ Redirect only if user is truly missing
-  if (!user || !user.name || !user.email) {
-    return <Navigate to="/log-in" state={{ from: location }} replace />;
-  }
-
-  return (
+  return user ? (
     <div className='w-full h-screen flex flex-col md:flex-row'>
       <div className='w-1/5 h-screen bg-white dark:bg-[#1f1f1f] sticky top-0 hidden md:block'>
         <Sidebar />
@@ -48,11 +30,14 @@ const Layout() {
 
       <div className='flex-1 overflow-y-auto'>
         <Navbar />
+
         <div className='p-4 2xl:px-10'>
           <Outlet />
         </div>
       </div>
     </div>
+  ) : (
+    <Navigate to='/log-in' state={{ from: location }} replace />
   );
 }
 

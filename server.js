a5bceead7d09dce5d9a624aa559ac093f30273aa
Default-ant <krishnamani468@gmail.com>
2025-09-30 12:00:37 +0000

    blank page error

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 app.use(express.static(path.join(__dirname, "dist")));
 
 // Fallback for React Router
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 

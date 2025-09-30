a1cf5eb9b9c5755fa60084583246954a151a4751
Default-ant <krishnamani468@gmail.com>
2025-09-30 12:35:27 +0000

    blank page error

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 
 export default defineConfig({
   base: "/",
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        redirects: resolve(__dirname, "public/_redirects"), // ✅ force include
-      },
-    },
-  },
 });

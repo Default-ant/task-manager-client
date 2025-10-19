e3dac5c760773ef5792ea508abdf045e94778736
Default-ant <krishnamani468@gmail.com>
2025-10-19 11:20:37 +0000

    /

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(history());
 app.use(express.static(path.resolve(__dirname, "dist")));
 
 // ✅ Catch-all route for deep links
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "dist", "index.html"));
 });
 
